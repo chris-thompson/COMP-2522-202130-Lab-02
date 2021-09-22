a9f54d089cbddfeabed27cefbc8e949fbced7389
Chris <chris_thompson@bcit.ca>
2021-09-22 13:32:35 -0700

    Ignore OUT folder

---
+++ src/ca/bcit/comp2522/lab02/HelloWorld.java
@@ -0,0 +1,18 @@
+package ca.bcit.comp2522.lab02;
+
+/**
+ * Greets the world.
+ *
+ * @author BCIT
+ * @version 2021
+ */
+public class HelloWorld {
+
+    /**
+     * Drives the program.
+     * @param args unused
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
